7da4a8760e74bb0781f25276ceec4760187f5f11
AgentSaxyCow <93024606+AgentSaxyCow@users.noreply.github.com>
2021-10-23 18:52:32 -0500

    Pushed updated movement and Main Scene

--- Assets/Scenes/Scripts/Eddie/Player.gd
+++ Assets/Scenes/Scripts/Eddie/Player.gd
@@ -1,41 +1,64 @@
 extends KinematicBody2D
 
 
-var direction = Vector2()
-var speed = 0
-var ACCELERATION = 500
-var DECCELERATION = 1000
-var axis = Vector2()
+var wheel_base = 50
 
-var motion = Vector2()
-var target_motion = Vector2()
-var steering = Vector2()
-const MASS = 2
+var steering_angle = 8
 
-const TOP_SPEED = 500
+var velocity = Vector2.ZERO
+var steer_angle
 
-var target_angle = 0
+var f_acc = 300
+var acc = Vector2.ZERO
+
+var friction = -0.4
+var drag = -0.0015
+var braking = -50
+var max_speed_reverse = 10
+
+var TOP_SPEED = 300
 
 func _physics_process(delta):
-	get_input_axis()
-	if axis == Vector2.ZERO:
-		apply_friction(DECCELERATION * delta)
-	else:
-		apply_movement(axis * ACCELERATION * delta)
-	motion = move_and_slide(motion)
-	look_at(position + motion)
+	acc = Vector2.ZERO
+	get_input()
+	apply_friction()
+	calculate_steering(delta)
+	velocity += acc * delta
+	velocity = move_and_slide(velocity)
 
-func get_input_axis():
-	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	return axis.normalized()
-	
-func apply_friction(amount):
-	if motion.length() > amount:
-		motion -= motion.normalized() * amount
-	else:
-		motion = Vector2.ZERO
+func apply_friction():
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	if velocity.length() < 100:
+		friction_force *= 3
+	acc += drag_force + friction_force
 
-func apply_movement(acceleration):
-	motion += acceleration
-	motion = motion.clamped(TOP_SPEED)
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("ui_right"):
+		turn += 1
+	if Input.is_action_pressed("ui_left"):
+		turn -= 1
+	steer_angle = turn * deg2rad(steering_angle)
+	if Input.is_action_pressed("ui_up"):
+		acc = transform.x * f_acc
+	if Input.is_action_pressed("ui_down"):
+		if velocity == Vector2.ZERO:
+			pass
+		else:
+			acc = transform.x * braking
+
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_angle) * delta
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		velocity = new_heading * velocity.length()
+	if d < 0:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+	rotation = new_heading.angle()
